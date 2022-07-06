039a054ff1ade4a536ee28ddd76a66a23eb895f0
azzurragraziano <azzurragraziano02@email.com>
2022-07-06 16:22:42 +0200

    repo init

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="title-container">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainTitle",
+    data() {
+        return {
+            title: 'La mia prima app con Vue CLI!'
+        };
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
